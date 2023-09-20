1075c71b6c79018ff3bcb38be8dbd80b8aa17076
JObV9n <ricardoshrestha@gmail.com>
2023-09-20 16:04:35 +0545

    Added LocalStorage to save the messages after the reload

--- laravel/public/js/chat.js
+++ laravel/public/js/chat.js
@@ -2,11 +2,13 @@
 const messageInput = document.getElementById('message-input');
 const chatBox = document.getElementById('chat-box');
 const sendButton= document.getElementById('send-button');
-sendButton.addEventListener('click', function(event) {
+
+sendButton.addEventListener('click', function() {
         const messageText = messageInput.value.trim();
         if (messageText !== '') {
             appendMessage('sent', messageText);
-            messageInput.value = '';
+            // messageInput.value = '';
+            saveToLocalStorage('sent',messageText);
         }
 });
 
@@ -22,3 +24,23 @@ function appendMessage(type, text) {
     chatBox.appendChild(messageDiv);
     chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom
 }
+
+//function to load messages in LocalStorage so that on every refresh or reload the  messages are not deleted
+
+//function to save the  message in local storage
+function loadFromLocalStorage() {
+    const savedMessages = JSON.parse(localStorage.getItem('local')) || [];
+    savedMessages.forEach(function (message) {
+        appendMessage(message.type, message.text);
+    });
+}
+function saveToLocalStorage(){
+    let type='sent';
+    let text=JSON.stringify(messageInput.value);//yesma JS object ma aunxa jun chai .stringfy le gareko ho
+    let finalText = JSON.parse(text); //tyo JS object lai String ma change gareko ho .parse le
+    const messagelocal = JSON.parse(localStorage.getItem('local')) || [];
+    messagelocal.push({ type,finalText });
+    localStorage.setItem('local',JSON.stringify(messagelocal));
+}
+//EventListener to listen when the window is reloaded or refreshed
+window.addEventListener('load',loadFromLocalStorage);

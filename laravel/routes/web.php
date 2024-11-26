6152d48cf030f100dc0477e10456a54a1a5aadda
JObV9n <ricardoshrestha@gmail.com>
2024-11-26 09:39:56 +0545

    migrate in Bun and Laravel-11

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,33 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get("chat", function () {
-    return "Chat Room and Chat Box";
+Route::get('/', function () {
+    return view('welcome');
 });
-
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    Route::get("history", function () {
-        return "Chat History";
-    });
-});
-Route::get('/chatroom', [HomeController::class, 'showUsers'])->name('chats');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Auth::routes();
 
